5aecd5edfc283e33faaad97ad2ac5d623c035cec
ycleo <leoyclee@gmail.com>
2022-02-12 14:38:02 +0800

    set cors rule

--- server/src/app.js
+++ server/src/app.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const app = express();
 const planetsRouter = require('./routes/planets/planets.router')
 
-// app.use(cors({ // middleware to set the cors rule
-//     origin: 'http://localhost:3000'
-// })); 
+app.use(cors({ // middleware to set the cors rule
+    origin: 'http://localhost:3000'
+})); 
 app.use(express.json()) // middleware that only parses json
 app.use(planetsRouter); // middleware to connect to planets router
 
--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const app = require('./app');
 // add express app middleware on top of the built-in http server
 const server = http.createServer(app);
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
+const PORT = 5000;
 
 server.listen(() => {
     console.log(`Server is listening on port ${PORT}...`)
