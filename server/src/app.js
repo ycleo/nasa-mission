17adeb33f7362e87e51f1b8ae095d4df5a7e8fbb
ycleo <leoyclee@gmail.com>
2022-02-16 01:14:31 +0800

    test ssh

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(cors({ // middleware to set the cors rule
     origin: 'http://localhost:3000'
 })); 
 app.use(morgan('combined')); // middleware to record the client log
-app.use(express.json()); // middleware that only parses json
+app.use(express.json());    // middleware that only parses json
 app.use(express.static(path.join(__dirname, '..', 'public'))); // middleware to serve static files
 app.use('/planets', planetsRouter); // middleware to connect to planets router
 app.use('/launches', launchesRouter); // middleware to connect to launches router
